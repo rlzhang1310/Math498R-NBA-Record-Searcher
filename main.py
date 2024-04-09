4607b3ef5939927c08d1440cc1a3da986715d7c0
Robert L Zhang <rzhang1@terpmail.umd.edu>
2024-04-09 00:56:11 -0400

    Added updates

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ from searcher import Searcher
 ################################################################
 
 def main():
-    data = pd.read_csv('nba_data/NBA Player Box Score Stats(1950 - 2022).csv')
+    data = pd.read_csv('data/NBA Player Box Score Stats(1950 - 2022).csv')
     searcher = Searcher(data)
-    print(searcher.search_max_score(1963))
+    # print(searcher.search_max_score(1963))
+    print(searcher.get_most_ftm_in1game_in_range('2000', '2022'))
 if __name__ == "__main__":
     main()
 
--- searcher.py
+++ searcher.py
@@ -8,6 +8,7 @@ import numpy as np
 class Searcher:
     def __init__(self, data):
         self.data = data
+        self.data['GAME_DATE'] = pd.to_datetime(self.data['GAME_DATE'])
 
 
     def search_max_score(self, from_year=1946, to_year=9999):
@@ -15,3 +16,12 @@ class Searcher:
         max_score = df_filter_year["PTS"].max()
         query_df = df_filter_year.query(f"PTS >= {max_score}")
         return query_df[["Season", "PLAYER_NAME", "Team", "GAME_DATE", "MATCHUP", "WL", "PTS"]]
+    
+    def get_most_ftm_in1game_in_range(self, start, end):
+        start = pd.to_datetime(start)
+        end = pd.to_datetime(end)
+        range = pd.date_range(start, end)
+        self.data = self.data[self.data['GAME_DATE'].isin(range)]
+        max_ftm_in_game = self.data['FTM'].max()
+        players_with_max = self.data.loc[self.data['FTM'] == max_ftm_in_game]
+        return players_with_max['PLAYER_NAME'].unique()
