c6d872780e43d4e76f667213336a16ff5e520cbc
tristenbs <letsgocatchaspider@gmail.com>
2024-04-16 22:47:40 -0400

    Add more search functions (stat lines)

--- searcher.py
+++ searcher.py
@@ -31,6 +31,41 @@ class Searcher:
         end = pd.to_datetime(end)
         range = pd.date_range(start, end)
         self.data = self.data[self.data['GAME_DATE'].isin(range)]
-        max_ftm_in_game = self.data['FGM'].max()
-        players_with_max = self.data.loc[self.data['FGM'] == max_ftm_in_game]
+        max_fgm_in_game = self.data['FGM'].max()
+        players_with_max = self.data.loc[self.data['FGM'] == max_fgm_in_game]
         return players_with_max['PLAYER_NAME'].unique()
+    
+    # how many times has a stat line been achieved?
+    # assists, blocks, shots
+    # might not work
+
+    def get_num_times_fgm_achieved(self, fgm) :
+        num = self.data.loc[self.data['FGM'] >= fgm]
+        return num
+    
+    def get_num_times_ftm_achieved(self, ftm) :
+        num = self.data.loc[self.data['FTM'] >= ftm]
+        return num
+    
+    def get_num_times_ast_achieved(self, ast):
+        # filter out non-tracked years
+        df = self.data.loc[(self.data['GAME_DATE'] >= '1970-01-01')]
+        num = df.loc[df['AST'] >= ast]
+        return num
+    
+    def get_num_times_blk_achieved(self, blk):
+        # filter out non-tracked years
+        df = self.data.loc[(self.data['GAME_DATE'] >= '1974-01-01')]
+        num = df.loc[df['BLK'] >= blk]
+        return num
+    
+    def get_num_times_reb_achieved(self, reb):
+        # filter out non-tracked years
+        num = self.data.loc[self.data['REB'] >= reb]
+        return num
+    
+    def get_num_times_stl_achieved(self, stl):
+        # filter out non-tracked years
+        df = self.data.loc[(self.data['GAME_DATE'] >= '1974-01-01')]
+        num = df.loc[df['STL'] >= stl]
+        return num
