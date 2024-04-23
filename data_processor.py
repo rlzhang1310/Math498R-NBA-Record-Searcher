164bac506928053fb0bd1c16718ab93dc9a56c5c
Robert L Zhang <rzhang1@terpmail.umd.edu>
2024-04-23 00:31:14 -0400

    added update method

--- data_processor.py
+++ data_processor.py
@@ -3,3 +3,12 @@ import pandas as pd
 ################################################################
 ## Add missing data with this if necessary
 ################################################################
+
+class Data_Processor:
+    def __init__(self, data):
+        self.data = data
+
+
+    def update(self, last_update, cur_time):
+        ## updates new data from the previous update
+        return
