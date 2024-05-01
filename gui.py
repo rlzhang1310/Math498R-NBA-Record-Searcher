5af44def8a5fa8383f97980b4eb70b726e383535
Robert L Zhang <rzhang1@terpmail.umd.edu>
2024-05-01 02:16:55 -0400

    added gui

--- gui.py
+++ gui.py
@@ -1,3 +1,48 @@
 ################################################################
 ## This file is responsible for the GUI
 ################################################################
+from searcher import Searcher
+from data_processor import DataProcessor
+from tkinter import *
+
+class GUI:
+    def __init__(self, searcher):
+        self.master = Tk()
+        self.master.title("NBA Record Searcher")
+        self.master.geometry("800x500")
+        self.master.configure(bg="white")
+
+        # Options Label
+        title_var = StringVar()
+        self.title_label = Label(self.master, text="Select Statistic")  
+        title_var.set("Select Statistic")
+        self.title_label.config(font =("Courier", 14))
+        # Radio button options
+        self.stats_options = ['STL', 'BLK', 'PTS', 'AST', 'REB']
+        self.title_label.grid(row=0, column=0, columnspan=4,pady=5)
+        self.selected_stat = IntVar() 
+
+        for index, stat in enumerate(self.stats_options):
+            button = Radiobutton(self.master, text=stat,
+                                 variable=self.selected_stat, value=index,
+                                 height=2, width=8)
+            row = 2  
+            column = index
+            button.grid(row=row, column=column, sticky='w', pady=5)
+
+        # Search button
+        self.find_button = Button(self.master, text="Find", command=self.execute_search)
+        self.find_button.grid(row=5, column=0, columnspan=1, pady=5)  # Span across two columns
+
+        # Reference to the searcher object
+        self.searcher = searcher
+        self.master.mainloop()
+
+    def execute_search(self):
+        stat = self.selected_stat.get()
+        # from_year = self.from_year_entry.get()
+        # to_year = self.to_year_entry.get()
+        print(stat)
+        # self.searcher.find(stat, from_year, to_year)
+
+
--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 import pandas as pd
 import numpy as np
+import tkinter as tk
 from searcher import Searcher
 from data_processor import DataProcessor
-
+from gui import GUI
 ################################################################
 ## Main file to run everything in
 ################################################################
 
 def main():
-    data = pd.read_csv('/home/tristen/MATH498R/Math498R-NBA-Record-Searcher/data/NBA Player Box Score Stats(1950 - 2022).csv')
+    data = pd.read_csv("/Users/rlzhang1310/Coding/MATH498R/Math498R-NBA-Record-Searcher/data/NBA Player Box Score Stats(1950 - 2022).csv")
+    # data = pd.read_csv('/home/tristen/MATH498R/Math498R-NBA-Record-Searcher/data/NBA Player Box Score Stats(1950 - 2022).csv')
     data_processor = DataProcessor(data)
     searcher = Searcher(data)
+    app = GUI(searcher)
+
     # print(searcher.search_max_score(1963))
-    print(searcher.get_most_stl_in1game_in_range('2000', '2022'))
+    # print(searcher.get_most_stl_in1game_in_range('2000', '2022'))
     # print(searcher.get_most_fgm_in1game_in_range('2000', '2022'))
-    print(searcher.get_num_times_ftm_achieved_in_range(10, '2000', '2022'))
+    # print(searcher.get_num_times_ftm_achieved_in_range(10, '2000', '2022'))
 
 if __name__ == "__main__":
     main()
